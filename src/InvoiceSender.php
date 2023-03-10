d053455098a2b64522732e341ce6157a71203daa
Marcos David Huaranga Ortega <dimacros@hotmail.com>
2023-03-09 20:00:02 -0500

    refactor InvoiceSender

--- src/InvoiceSender.php
+++ src/InvoiceSender.php
@@ -3,13 +3,67 @@
 namespace EBilling;
 
 use EBilling\Domain\Invoice;
+use EBilling\InvoiceFormatter\FactPseFormatter;
+use EBilling\InvoiceFormatter\PseFormatter;
 
-interface InvoiceSender
+final class InvoiceSender
 {
-    const FACTPSE = 'factpse';
-    const PSE = 'pse';
+    private $url;
 
-    public function getRequestDetails();
+    private $token;
 
-    public function send(Invoice $invoice);
+    private $requestDetails;
+
+    public function __construct($url, $token)
+    {
+        $this->url = $url;
+        $this->token = $token;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    public function getRequestDetails()
+    {
+        return $this->requestDetails;
+    }
+
+    public function send(Invoice $invoice)
+    {
+        $isFactPse = \str_contains($this->url, 'factpse.com');
+        $formatter = $isFactPse ? new FactPseFormatter($invoice) : new PseFormatter($invoice);
+        $json = json_encode($formatter->toArray());
+        $handler = curl_init($this->url);
+        $headers = [
+            'Content-Type: application/json; charset=utf-8',
+            'Content-Length: ' . strlen($json),
+            'Authorization: Bearer ' . $this->token
+        ];
+
+        (! $isFactPse) && array_push($headers, 'x-access-token: ' . $this->token);
+
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($handler, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($handler);
+
+        curl_close($handler);
+
+        $this->requestDetails = [
+            'url' => $this->url,
+            'token' => $this->token,
+            'body' => $formatter->toArray()
+        ];
+
+        return $result;
+    }
 }
